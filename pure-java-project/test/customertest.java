66aec8891ad42df7243f664506a89cf76f0f3ac3
ManhHoa2405 <manhhoa02042005@gmail.com>
2025-05-15 08:41:32 +0700

    .

--- pure-java-project/test/customertest.java
+++ pure-java-project/test/customertest.java
@@ -1,5 +1,5 @@
-public class TestKhachHang {
-    public static void main(String[] args) {
+public class customertest {
+    public void test {
         customer customer01 = new customer(01,"Nguyen Manh Hoa","123455@gmail.com",0129374837);
         System.out.println(customer01.ma_kh + customer01.ten_kh + customer01.email + customer01.so_dien_thoai);
     }
---
+++ pure-java-project/test/main.java
@@ -0,0 +1,4 @@
+public static void main(String args[]){
+    customer customer01 = new customer(01,"Nguyen Manh Hoa","123455@gmail.com",0129374837);
+    System.out.println(customer01.ma_kh + customer01.ten_kh + customer01.email + customer01.so_dien_thoai);
+}
