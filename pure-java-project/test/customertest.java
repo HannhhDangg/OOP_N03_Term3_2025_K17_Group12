c3f9b719fcc741900a48fe0f7bd93949bb4af21a
ManhHoa2405 <manhhoa02042005@gmail.com>
2025-05-15 09:05:25 +0700

    .

--- pure-java-project/test/customertest.java
+++ pure-java-project/test/customertest.java
@@ -4,6 +4,9 @@ import main.java.model.customer;
 public class customertest {
     public void test {
         customer customer01 = new customer(01,"Nguyen Manh Hoa","123455@gmail.com",0129374837);
-        System.out.println(customer01.ma_kh + customer01.ten_kh + customer01.email + customer01.so_dien_thoai);
+        System.out.println("ma khach hang : " + customer01.ma_kh);
+        System.out.println("ten khach hang : " + customer01.ten_kh);
+        System.out.println("email cua khach hang : " + customer01.email);
+        System.out.println("so dien thoai : " + customer01.so_dien_thoai);
     }
 }
