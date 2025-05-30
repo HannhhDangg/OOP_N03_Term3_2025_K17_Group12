752c06d302dd3a14b313b4fb1c675c2d9d48a509
Nguyendat0819 <23010282@st.phenikaa-uni.edu.vn>
2025-05-30 08:03:19 +0000

    done fix

--- pure-java-project/test/ProductListTest.java
+++ pure-java-project/test/ProductListTest.java
@@ -1,22 +1,22 @@
 public class ProductListTest {
     public static void main(String[] args) {
-        productsList list = new productsList();
+        ProductsList list = new ProductsList();
 
         // Thêm sản phẩm
-        list.addmaSP(new maSP(1, "Áo thun", 10));
-        list.addmaSP(new maSP(2, "Quần jeans", 5));
-        list.addmaSP(new maSP(3, "Giày", 3));
+        list.themSanPham(new Product("SP01", "Áo thun", "Áo", "M", "Trắng", 150000, 10));
+        list.themSanPham(new Product("SP02", "Quần jeans", "Quần", "L", "Xanh", 300000, 5));
+        list.themSanPham(new Product("SP03", "Giày", "Giày", "42", "Đen", 500000, 3));
 
         System.out.println("=== Danh sách ban đầu ===");
-        list.printProductList();
+        list.inDanhSachSanPham();
 
         // Sửa tên sản phẩm có số lượng 5
-        list.getEditmaSP("Quần kaki", 5);
+        list.suaTenSanPhamTheoSoLuong("Quần kaki", 5);
 
         // Xoá sản phẩm có số lượng 3
-        list.getDeletemaSP(3);
+        list.xoaSanPhamTheoSoLuong(3);
 
         System.out.println("=== Danh sách sau khi sửa và xoá ===");
-        list.printProductList();
+        list.inDanhSachSanPham();
     }
 }
